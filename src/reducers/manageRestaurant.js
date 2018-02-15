bbb643c7ecd4ce9a9cdb5893ceb42fd344cd3a72
jeffrey henderson <jeffrey.henderson17@gmail.com>
2018-02-15 18:23:56 +0000

    automatically backed up by learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,14 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants(state = {
+    restaurants: [],
+    reviews: []
+  }, action) {
+  switch(action.type) {
+    case "ADD_RESTAURANT":
+      return [...state, action.restaurant]
+    default: 
+      return state
+  }
 }
